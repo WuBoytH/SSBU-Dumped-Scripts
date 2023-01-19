a0e80cf7ce12e9ee48a0022f6fb1e7ccf934d82c
WuBoytH <alexwuief@gmail.com>
2023-01-19 09:13:01 -0500

    Reorganization

--- mass_converter.py
+++ mass_converter.py
@@ -8,23 +8,15 @@ def collect_dirs(dir: str):
     entries = os.listdir(dir)
     for entry in entries:
         # ignore status script folders
-        if "Status Scripts" in entry:
-            continue
-        if ".git" in entry:
-            continue
-        if "smashline" in entry:
-            continue
-        
-        entry = os.path.join(dir, entry)
-
-        if os.path.isdir(entry):
-            print("adding: " + entry)
-            
-            for subdir in os.listdir(entry):
-                print("checking subdir: " + os.path.join(entry, subdir))
-                if os.path.isdir(os.path.join(entry, subdir)):
-                    dirs.append(os.path.join(entry, subdir))
-                    collect_dirs(os.path.join(entry, subdir))
+        if "skyline-acmd" in entry:
+            if os.path.isdir(entry):
+                print("adding: " + entry)
+                
+                for subdir in os.listdir(entry):
+                    print("checking subdir: " + os.path.join(entry, subdir))
+                    if os.path.isdir(os.path.join(entry, subdir)):
+                        dirs.append(os.path.join(entry, subdir))
+                        collect_dirs(os.path.join(entry, subdir))
 
 
 
